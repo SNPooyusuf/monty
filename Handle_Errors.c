037d54e5d9404841adad567deba90ae43195ef27
SNPooyusuf <yosefosama902@gmail.com>
2023-12-14 09:52:35 -0800

    fixed

--- Handle_Errors.c
+++ Handle_Errors.c
@@ -1,90 +1,86 @@
 #include "monty.h"
 
 /**
- * Errors - Prints appropiate error messages determined by their error code.
+ * Errors - Prints appropriate error messages determined by their error code.
  * @error_code: The error codes are the following:
  * (1) -> The user does not give any file or more than one file to the program.
  * (2) -> The file provided is not a file that can be opened or read.
  * (3) -> The file provided contains an invalid instruction.
  * (4) -> When the program is unable to malloc more memory.
  * (5) -> When the parameter passed to the instruction "push" is not an int.
- * (6) -> When the stack it empty for pint.
- * (7) -> When the stack it empty for pop.
- * (8) -> When stack is too short for operation.
+ * (6) -> When the stack is empty for pint.
+ * (7) -> When the stack is empty for pop.
+ * (8) -> When the stack is too short for operation.
  */
 void Errors(int error_code, ...)
 {
-    va_list ag;
-    char *optimal;
-    int L_Nums;
+	va_list ag;
+	char *optimal;
+	int L_Nums;
 
-    va_start(ag, error_code);
-    switch (error_code)
-    {
-    case 1:
-        fprintf(stderr, "USAGE: monty file\n");
-        break;
-    case 2:
-        fprintf(stderr, "Error: Can't open file %s\n",
-                va_arg(ag, char *));
-        break;
-    case 3:
-        L_Nums = va_arg(ag, int);
-        optimal = va_arg(ag, char *);
-        fprintf(stderr, "L%d: unknown instruction %s\n", L_Nums, optimal);
-        break;
-    case 4:
-        fprintf(stderr, "Error: malloc failed\n");
-        break;
-    case 5:
-        fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
-        break;
-    default:
-        break;
-    }
-    free_nodes();
-    exit(EXIT_FAILURE);
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+	case 1:
+		fprintf(stderr, "USAGE: monty file\n");
+		break;
+	case 2:
+		fprintf(stderr, "Error: Can't open file %s\n", va_arg(ag, char *));
+		break;
+	case 3:
+		L_Nums = va_arg(ag, int);
+		optimal = va_arg(ag, char *);
+		fprintf(stderr, "L%d: unknown instruction %s\n", L_Nums, optimal);
+		break;
+	case 4:
+		fprintf(stderr, "Error: malloc failed\n");
+		break;
+	case 5:
+		fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+		break;
+	default:
+		break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
  * More_Errors - handles errors.
  * @error_code: The error codes are the following:
- * (6) -> When the stack it empty for pint.
- * (7) -> When the stack it empty for pop.
- * (8) -> When stack is too short for operation.
+ * (6) -> When the stack is empty for pint.
+ * (7) -> When the stack is empty for pop.
+ * (8) -> When the stack is too short for operation.
  * (9) -> Division by zero.
  */
 void More_Errors(int error_code, ...)
 {
-    va_list ag;
-    char *Optimal;
-    int L_Nums;
+	va_list ag;
+	char *Optimal;
+	int L_Nums;
 
-    va_start(ag, error_code);
-    switch (error_code)
-    {
-    case 6:
-        fprintf(stderr, "L%d: can't pint, stack empty\n",
-                va_arg(ag, int));
-        break;
-    case 7:
-        fprintf(stderr, "L%d: can't pop an empty stack\n",
-                va_arg(ag, int));
-        break;
-    case 8:
-        L_Nums = va_arg(ag, unsigned int);
-        Optimal = va_arg(ag, char *);
-        fprintf(stderr, "L%d: can't %s, stack too short\n", L_Nums, Optimal);
-        break;
-    case 9:
-        fprintf(stderr, "L%d: division by zero\n",
-                va_arg(ag, unsigned int));
-        break;
-    default:
-        break;
-    }
-    free_nodes();
-    exit(EXIT_FAILURE);
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+	case 6:
+		fprintf(stderr, "L%d: can't pint, stack empty\n", va_arg(ag, int));
+		break;
+	case 7:
+		fprintf(stderr, "L%d: can't pop an empty stack\n", va_arg(ag, int));
+		break;
+	case 8:
+		L_Nums = va_arg(ag, unsigned int);
+		Optimal = va_arg(ag, char *);
+		fprintf(stderr, "L%d: can't %s, stack too short\n", L_Nums, Optimal);
+		break;
+	case 9:
+		fprintf(stderr, "L%d: division by zero\n", va_arg(ag, unsigned int));
+		break;
+	default:
+		break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
 }
 
 /**
@@ -102,14 +98,14 @@ void Errors_Strings(int error_code, ...)
 	l_num = va_arg(ag, int);
 	switch (error_code)
 	{
-		case 10:
-			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
-			break;
-		case 11:
-			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
-			break;
-		default:
-			break;
+	case 10:
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+		break;
+	case 11:
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+		break;
+	default:
+		break;
 	}
 	free_nodes();
 	exit(EXIT_FAILURE);
--- main.c
+++ main.c
@@ -2,46 +2,47 @@
 stack_t *head = NULL;
 
 /**
- * main - entry point
- * @argc: arguments count
- * @argv: list of arguments
- * Return: always 0
+ * main - Entry point
+ * @argc: Arguments count
+ * @argv: List of arguments
+ * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    open_file(argv[1]);
-    free_nodes();
-    return (0);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
 }
 
 /**
  * add_to_queue - Adds a node to the queue.
  * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
+ * @ln: Line number of the opcode (unused).
  */
 void add_to_queue(stack_t **new_node, __attribute__((unused)) unsigned int ln)
 {
-    stack_t *Temp;
+	stack_t *Temp;
 
-    if (new_node == NULL || *new_node == NULL)
-        exit(EXIT_FAILURE);
-    if (head == NULL)
-    {
-        head = *new_node;
-        return;
-    }
-    Temp = head;
-    while (Temp->next != NULL)
-        Temp = Temp->next;
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
 
-    Temp->next = *new_node;
-    (*new_node)->prev = Temp;
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+
+	Temp = head;
+	while (Temp->next != NULL)
+		Temp = Temp->next;
+
+	Temp->next = *new_node;
+	(*new_node)->prev = Temp;
 }
 
 /**
@@ -49,33 +50,35 @@ void add_to_queue(stack_t **new_node, __attribute__((unused)) unsigned int ln)
  */
 void free_nodes(void)
 {
-    stack_t *Temp;
+	stack_t *Temp;
 
-    if (head == NULL)
-        return;
+	if (head == NULL)
+		return;
 
-    while (head != NULL)
-    {
-        Temp = head;
-        head = head->next;
-        free(Temp);
-    }
+	while (head != NULL)
+	{
+		Temp = head;
+		head = head->next;
+		free(Temp);
+	}
 }
 
 /**
  * create_node - Creates a node.
  * @n: Number to go inside the node.
- * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ * Return: Upon success, a pointer to the node. Otherwise NULL.
  */
 stack_t *create_node(int n)
 {
-    stack_t *node;
+	stack_t *node;
 
-    node = malloc(sizeof(stack_t));
-    if (node == NULL)
-        Errors(4);
-    node->next = NULL;
-    node->prev = NULL;
-    node->n = n;
-    return (node);
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		Errors(4);
+
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+
+	return (node);
 }
